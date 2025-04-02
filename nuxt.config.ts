6baf7e89d843ec022d1f2a577a1ded46fa26b01e
WarrenAdams8 <warren.codes@outlook.com>
2025-04-02 09:30:26 -0400

    start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/supabase"],
+  css: ["@picocss/pico/css/pico.css"],
+  supabase: {
+    redirect: false,
+  },
+});
